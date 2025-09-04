58124e72195242278fb65525c61ea45c555d19f3
daffaamf <daffamaulana170502@gmail.com>
2025-09-04 21:54:46 +0700

    challenge

--- src/main/java/com/sqabatch24/javadasar/App.java
+++ src/main/java/com/sqabatch24/javadasar/App.java
@@ -6,27 +6,34 @@ import java.util.Scanner;
 public class App {
 
     public static void main(String[] args) {
-        int[][] myArray = {
+        // Challenge
+
+        // berikut adalah map untuk game
+        // map yang sisa bagi 3 = 0 adalah
+        // tempat enemy berada, hindari map itu
+        // spawn -> nyatet letak koordinat musuh (x % 3 = 0).
+        // catatan spawn di cetak
+
+        int[][] gameMap = {
                 { 1, 2, 3, 4 },
                 { 5, 6, 7, 8 },
                 { 9, 10, 11, 12 },
                 { 13, 14, 15, 16 }
         };
 
-        System.out.println(myArray[1][2]);
-        System.out.println(myArray[3][2]);
+        String musuh = ""; 
 
-        for (int[] baris : myArray) {
-            for (int value : baris) {
-                System.out.println(value);
+         for (int baris = 0; baris < gameMap.length; baris++) {
+            for (int kolom = 0; kolom < gameMap[baris].length; kolom++) {
+                int cell = gameMap[baris][kolom];
+                if (cell % 3 == 0) {
+                    musuh += "(" + baris + "," + kolom + ") ";
+                } else {
+                    System.out.println("Player jalan ke (" + baris + "," + kolom + ") nilai = " + cell);
+                }
             }
         }
-
-        for (int baris = 0; baris < myArray.length; baris++) {
-            for (int kolom = 0; kolom < myArray[baris].length; kolom++) {
-                int cell = myArray[baris][kolom];
-                System.out.println("Baris ke " + baris + " Kolom ke " + kolom + " Nilainya adalah = " + cell);
-            }
-        }
+        System.out.println("\nKoordinat musuh yang harus dihindari:");
+        System.out.println(musuh);
     }
 }
