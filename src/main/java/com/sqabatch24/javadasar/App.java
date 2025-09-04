4d5bd9776c5d68cbb47a55c3e7e0c4b10bf415b1
daffaamf <daffamaulana170502@gmail.com>
2025-09-04 19:56:52 +0700

    ok

--- src/main/java/com/sqabatch24/javadasar/App.java
+++ src/main/java/com/sqabatch24/javadasar/App.java
@@ -1,37 +1,14 @@
 package com.sqabatch24.javadasar;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        int[] nilai = {
-                1, // 0
-                2, // 1
-                3, // 2
-                4, // 3
-                5, // 4
-                6, // 5
-                7, // 6
-                8, // 7
-                9, // 8
-                10, // 9
-                11, // 10
-                12, // 11
-        };
+        Scanner sn = new Scanner(System.in);
 
-        for (int i = 0; i < nilai.length; i++) {
-            System.out.println(("Nilai ke" + (i + 1) + "adalah = " + nilai[i]));
-        }
-
-        String[] daftarBelanja = {
-            "Pepsovent",
-            "Lafebuoy",
-            "Lada bubuk",
-            "Bawang putih",
-            "Reenzo",
-        };
-
-        for (String barang : daftarBelanja){
-            System.out.println(barang);
-        }
+        System.out.print("Masukkan Harga: ");
+        int tampungan = sn.nextInt();
+        System.out.println(tampungan);
     }
 }
