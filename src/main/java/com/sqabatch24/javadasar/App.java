7219f97d3cada05a3d444bd88cc2a1be1cdc7a40
daffaamf <daffamaulana170502@gmail.com>
2025-09-04 20:55:56 +0700

    done

--- src/main/java/com/sqabatch24/javadasar/App.java
+++ src/main/java/com/sqabatch24/javadasar/App.java
@@ -1,30 +1,32 @@
 package com.sqabatch24.javadasar;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class App {
 
     public static void main(String[] args) {
-        Scanner sn = new Scanner(System.in);
+        int[][] myArray = {
+                { 1, 2, 3, 4 },
+                { 5, 6, 7, 8 },
+                { 9, 10, 11, 12 },
+                { 13, 14, 15, 16 }
+        };
 
-        ArrayList<Integer> tampungan = new ArrayList<>();
-        int total = 0;
+        System.out.println(myArray[1][2]);
+        System.out.println(myArray[3][2]);
 
-
-        for (int i = 0; i < 10; i++) {
-            System.out.print("Masukkan Harga: ");
-            int harga = sn.nextInt();
-
-        if (harga == 99){
-            break;
+        for (int[] baris : myArray) {
+            for (int value : baris) {
+                System.out.println(value);
+            }
         }
 
-        tampungan.add(harga);
-        total += harga;
+        for (int baris = 0; baris < myArray.length; baris++) {
+            for (int kolom = 0; kolom < myArray[baris].length; kolom++) {
+                int cell = myArray[baris][kolom];
+                System.out.println("Baris ke " + baris + " Kolom ke " + kolom + " Nilainya adalah = " + cell);
+            }
         }
-
-        System.out.println("Daftar harga: " + tampungan);
-        System.out.println("Total Harga: Rp " + total);
     }
 }
